e5efc6bf0e944b6121efabde67ff53799266c416
pmg369 <pablomarting19@gmail.com>
2024-12-10 18:10:08 -0300

    update menu overlay and main image

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menuButton = document.getElementById('menu-button');
 const menu = document.getElementById('menu');
+const menuOverlay = document.getElementById('menu-overlay');
 
 // Estado inicial del menú (cerrado)
 let isMenuOpen = false;
@@ -10,8 +11,18 @@ menuButton.addEventListener('click', () => {
     if (isMenuOpen) {
         menu.classList.add('scale-y-100');
         menu.classList.remove('scale-y-0');
+        menuOverlay.classList.add('active'); // Mostrar overlay
     } else {
         menu.classList.add('scale-y-0');
         menu.classList.remove('scale-y-100');
+        menuOverlay.classList.remove('active'); // Ocultar overlay
     }
+});
+
+// Cerrar el menú al hacer clic en el overlay
+menuOverlay.addEventListener('click', () => {
+    isMenuOpen = false; // Asegurarse de cerrar el menú
+    menu.classList.add('scale-y-0');
+    menu.classList.remove('scale-y-100');
+    menuOverlay.classList.remove('active'); // Ocultar overlay
 });
