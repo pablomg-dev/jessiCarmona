53c9c0dead8f0b246dd4a7bb7491e38cf6e32324
pmg369 <pablomarting19@gmail.com>
2024-12-11 18:07:24 -0300

    modify colors btn menu home

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
+
+// Menu hamburguesa
 const menuButton = document.getElementById('menu-button');
 const menu = document.getElementById('menu');
 const menuOverlay = document.getElementById('menu-overlay');
 
-// Estado inicial del menú (cerrado)
 let isMenuOpen = false;
 
-menuButton.addEventListener('click', () => {
-    isMenuOpen = !isMenuOpen; // Alternar el estado
+function toggleMenu() {
+    isMenuOpen = !isMenuOpen;
 
-    if (isMenuOpen) {
-        menu.classList.add('scale-y-100');
-        menu.classList.remove('scale-y-0');
-        menuOverlay.classList.add('active'); // Mostrar overlay
-    } else {
-        menu.classList.add('scale-y-0');
-        menu.classList.remove('scale-y-100');
-        menuOverlay.classList.remove('active'); // Ocultar overlay
-    }
-});
+    menuButton.classList.toggle('open', isMenuOpen); // Alternar clase 'open'
+    menu.classList.toggle('scale-y-100', isMenuOpen);
+    menu.classList.toggle('scale-y-0', !isMenuOpen);
+    menuOverlay.classList.toggle('active', isMenuOpen);
+}
 
-// Cerrar el menú al hacer clic en el overlay
+menuButton.addEventListener('click', toggleMenu);
 menuOverlay.addEventListener('click', () => {
-    isMenuOpen = false; // Asegurarse de cerrar el menú
-    menu.classList.add('scale-y-0');
-    menu.classList.remove('scale-y-100');
-    menuOverlay.classList.remove('active'); // Ocultar overlay
+    if (isMenuOpen) toggleMenu();
 });
 
 // Animacion texto home
