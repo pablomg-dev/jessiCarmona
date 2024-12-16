f24e54f3a3abd4177cfa4e93e4ecbbde09102d5f
pmg369 <pablomarting19@gmail.com>
2024-12-16 17:41:27 -0300

    update backgraound image

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-
 // Menu hamburguesa
 const menuButton = document.getElementById('menu-button');
 const menu = document.getElementById('menu');
-const menuOverlay = document.getElementById('menu-overlay');
 
 let isMenuOpen = false;
 
@@ -12,13 +10,9 @@ function toggleMenu() {
     menuButton.classList.toggle('open', isMenuOpen); // Alternar clase 'open'
     menu.classList.toggle('scale-y-100', isMenuOpen);
     menu.classList.toggle('scale-y-0', !isMenuOpen);
-    menuOverlay.classList.toggle('active', isMenuOpen);
 }
 
 menuButton.addEventListener('click', toggleMenu);
-menuOverlay.addEventListener('click', () => {
-    if (isMenuOpen) toggleMenu();
-});
 
 // Animacion texto home
 document.addEventListener('DOMContentLoaded', () => {
