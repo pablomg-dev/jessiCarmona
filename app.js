1bc52031b5d179e73e515bc3decb135ece08b8a8
pablomg-dev <pablomartingonzalez2025@gmail.com>
2025-01-29 22:45:19 -0300

    update carousel

--- app.js
+++ app.js
@@ -13,3 +13,24 @@ function toggleMenu() {
 }
 
 menuButton.addEventListener('click', toggleMenu);
+
+// Carrousel home
+const carousel = document.querySelector(".carousel-inner");
+const items = document.querySelectorAll(".carousel-item");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+let index = 0;
+
+function updateCarousel() {
+    carousel.style.transform = `translateX(-${index * 100}%)`;
+}
+
+next.addEventListener("click", () => {
+    index = (index + 1) % items.length;
+    updateCarousel();
+});
+
+prev.addEventListener("click", () => {
+    index = (index - 1 + items.length) % items.length;
+    updateCarousel();
+});
